5e759a149b946f0fd747e7b6c836f34161561c35
Jo-Marie <jomarierossouw@gmail.com>
2026-05-07 16:32:49 +0200

    Elliott fitting code geskryf, asook arrhenius en bose-einstein.

--- src/optical_jomarie/absorption/elliott/elliott.py
+++ src/optical_jomarie/absorption/elliott/elliott.py
@@ -22,7 +22,7 @@ def O0(alpha, eV, Ry):
 def Exo_spectra(Ry, n, alpha):
     return (Ry * euler_L(n+alpha-2))/(math.factorial(n-1) * (n+(alpha-3)/2)**(alpha+1)) 
 
-def dirac_broadening(Ry, Eg, eV, a, alpha):
+def dirac_broadening(Ry, Eg, eV, n, a, alpha):
     return 1/(a*np.sqrt(np.pi)) * np.exp(-1 * np.power( ( eV-Eg-(Ry)/(np.power( n+(alpha-3)/2, 2 )) )/a, 2 ))
 
 def cont_spectra(alpha, gamma):
@@ -35,25 +35,24 @@ def heavi_broadening(eV, Eg, b):
 
 # estimates alpha, Ry, En
 # constant n, a, en b
-def elliott(a, b, n, alpha, Eg, Ry, eV):
+def elliott(eV, a, b, n, alpha, Eg, Ry):
     Exo = 0
-    for n in range(n):
+    for i in range(1, int(n) +1):
         n = n+1 #begin by n=1 ipv n=0 want anders doen mens (-1)! which DNE
-        E_n = En(Eg, Ry, n, alpha)
-        Exo += Exo_spectra(Ry,n,alpha)*dirac_broadening(Ry, Eg, eV, a, alpha)
+        E_n = En(Eg, Ry, i, alpha)
+        Exo += Exo_spectra(Ry,i,alpha)*dirac_broadening(Ry, Eg, eV, n, a, alpha)
 
     O_0 = O0(alpha, eV, Ry)
-    absorption = O_0*(Exo + cont_spectra(alpha, gamma(Ry, eV))*heavi_broadening(eV, Eg, b))
-    Eb = -Ry/(2+(alpha-3)/2)**2
-    return absorption, Exo, Eb, E_n
+    return (Exo + cont_spectra(alpha, gamma(Ry, eV))*heavi_broadening(eV, Eg, b))
 
 if __name__ == '__main__':
     eV = np.linspace(1.5, 3.5, 1000)
     alpha, a, b, Eg, Ry, n = 2.75, 0.058, 1.79, 2.51, 0.089, 1
-    absorp, Exo, Eb, E_n = elliott(a, b, n, alpha, Eg, Ry, eV)
-    cont = cont_spectra(alpha, gamma(Ry, eV))*heavi_broadening(eV, Eg, b)
-    print(Eb, E_n)
+    absorp = elliott(eV, a, b, n, alpha, Eg, Ry)
+    #cont = cont_spectra(alpha, gamma(Ry, eV))*heavi_broadening(eV, Eg, b)
+    #print(Eb, E_n)
     #plt.plot(eV, Exo, '--k')
+    print(absorp)
     plt.plot(eV, absorp)
     plt.show()
      
